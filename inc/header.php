0ff3ee4e3b8074652ffa73a294e3ddab778fa1b8
Laurarman <laura.arman@gmail.com>
2017-05-12 21:56:17 +0100

    Create header.php
    copied header info from

---
+++ inc/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><!-- Design by Free CSS Templates http://www.freecsstemplates.org Released for free under a Creative Commons Attribution License Name : Laura Arman Version : 1.0 Released : 20130222 -->
+
+<meta name="keywords" content="Welsh,Cymraeg,Linguistics,ieithyddiaeth" />
+<meta name="description" content="Laura Arman" />
+<meta name="author" content="Laura Arman" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" /><title>Laura Arman - School of Linguistics and English Language</title>
+
+<link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="style.css" /><!-- Google analytics javascript-->
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-39775262-1', 'manchester.ac.uk');
+ga('send', 'pageview');
+</script>
+<style type="text/css">
+<!--
+.style1 {
+font-size: 16px;
+font-weight: bold;
+}
+.style2 {
+font-size: 12pt;
+font-weight: bold;
+}
+-->
+</style></head>
+<body>
+<a><!-- Google analytics javascript-->
+</a>
+<div id="bg">
+<div id="outer">
+<div id="logo"> <a href="http://laurarman.github.io">
+<h1 align="left"> Laura Arman </h1>
+</a></div>
+<div id="banner"> <a href="http://www.arpat.net/blog/wp-content/gallery/1009snowdonia/snowdonia-15big.jpg">
+<img src="images/snowdonia-banner.jpg" alt="" height="212" width="1124" /></a> </div>
+<div id="nav">
+<ul>
+<li class="current_page_item"> <a href="index.html">About</a></li>
+<li><a href="Cymraeg.htm">Cymraeg</a></li>
+<li> <a href="cv-web-en-la.pdf">CV</a> </li>
+<li> <a href="Projects.htm">Projects</a> </li>
+<li> <a href="Output.htm">Output</a></li>
+<li> <a href="Data.htm">Data</a></li>
+<li class="last"> <a href="Teaching.htm">Teaching</a></li>
+</ul>
+</div>
